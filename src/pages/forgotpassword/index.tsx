1b82bdb167596a281e90babf08d8a550fb6b6dba
Marcu Operario <marcuoperario@gmail.com>
2023-08-27 03:36:18 +0800

    feat: add ForgotPassword page

---
+++ src/pages/forgotpassword/index.tsx
@@ -0,0 +1,12 @@
+import ForgotPasswordForm from '../../components/forgotpassword/form';
+import AuthRouteLayout from '../../components/layouts/auth';
+
+const ForgotPassword = () => {
+  return (
+    <AuthRouteLayout>
+      <ForgotPasswordForm />
+    </AuthRouteLayout>
+  )
+}
+
+export default ForgotPassword;
