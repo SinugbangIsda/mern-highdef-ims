20973f75e9ef3a885278960dd511095f0333f1da
Marcu Operario <marcuoperario@gmail.com>
2023-08-27 03:38:32 +0800

    feat: add authSlice

---
+++ src/redux/slices/authSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { User } from "../../interfaces";
+
+interface AuthState  {
+    token: string | null;
+    user: User | null;
+};
+
+const initialState: AuthState = {
+    token: null,
+    user: null
+};
+
+export const getUserData = () => {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+    
+    if (token && user) {
+        return { token, user };
+    };
+    
+    return initialState;
+};
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState: getUserData(),
+    reducers: {
+        setAuth(state, action) {
+            const { user, token } = action.payload;
+            localStorage.setItem("token", JSON.stringify(token));
+            localStorage.setItem("user", JSON.stringify(user));
+            return { ...state, user, token };
+        },
+        logOut: () => {
+            localStorage.removeItem("token"),
+            localStorage.removeItem("user");
+            return initialState;
+        },
+    }
+});
+
+export const { setAuth, logOut } = authSlice.actions;
+export default authSlice.reducer;
