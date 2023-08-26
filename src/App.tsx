56467b747f46d6e56c5abe690c99592b7472c4e8
Marcu Operario <marcuoperario@gmail.com>
2023-08-27 05:45:33 +0800

    feat: add Transactions route

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import AuthGuard from "./components/auth";
 import Users from "./pages/users";
 import ThemeProvider from "./components/theme/provider";
 import { ToastContainer } from "react-toast";
+import Transactions from "./pages/transactions";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Dashboard />
+      },
+      {
+        path: "/transactions",
+        element: <Transactions />
       },
       {
         path: "/users",
