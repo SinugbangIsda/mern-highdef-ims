76d338d4a48dd74e71a020eeffe45233874fb312
Marcu Operario <marcuoperario@gmail.com>
2023-08-27 03:35:49 +0800

    feat: add Signup page

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,12 @@
+import AuthRouteLayout from "../../components/layouts/auth";
+import SignupForm from "../../components/signup/form";
+
+const Signup = () => {
+    return (
+        <AuthRouteLayout>
+            <SignupForm />
+        </AuthRouteLayout>
+    )
+};
+
+export default Signup;
