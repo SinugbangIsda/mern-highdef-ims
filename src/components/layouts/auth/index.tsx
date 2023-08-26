7e39a79bdd39277dc9a310ce4c2d7ed0d43b9487
Marcu Operario <marcuoperario@gmail.com>
2023-08-27 03:34:16 +0800

    feat: add AuthRouteLayout

---
+++ src/components/layouts/auth/index.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode } from 'react';
+import Logo from "../../../assets/images/hd-lgo.svg";
+
+interface AuthRouteLayoutProps {
+  children: ReactNode;
+}
+
+const AuthRouteLayout = ({ children }: AuthRouteLayoutProps) => {
+  return (
+    <section className = "h-screen flex flex-col m-auto">
+      <div className = "flex flex-col items-center justify-center grow bg-[#F3F4F9] space-y-6 p-4 dark:bg-[#11151C]">
+        <div className =  "w-full bg-white dark:bg-[#222938] dark:text-white rounded-lg shadow sm:max-w-xl space-y-4 border dark:border-[#363D4C]">
+          <div className = "p-10 space-y-4">
+            <div className = "w-full flex flex-col items-center space-y-2">
+              <img 
+                src = { Logo }
+                alt = "Company Logo"
+              />
+              <h1 className = "font-bold text-[#3E78BC] text-3xl">
+                HD LOGISTICS
+              </h1>
+            </div>
+            { children }
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AuthRouteLayout;
