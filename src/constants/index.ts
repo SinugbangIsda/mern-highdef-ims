bd673ae62af08e542476cec4541e93ae3ad23f75
Marcu Operario <marcuoperario@gmail.com>
2023-09-10 00:34:51 +0800

    chore: add global connstants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,25 @@
-export const BASE_URL = "https://highdef-api.vercel.app/api/v1";
+export const BASE_URL = {
+    api: "https://highdef-api.vercel.app/api/v1"
+};
+
+export const transactions_table_headers = [
+    "transaction_name",
+    "total_price",
+    "createdAt",
+    "updatedAt"
+];
+
+export const users_table_headers =  [
+    "firstname",
+    "lastname",
+    "email_address",
+    "role"
+];
+
+export const produccts_table_headers = [
+    "name",
+    "price",
+    "quantity",
+    "date"
+];
+  
